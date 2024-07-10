f1e8e34ed265a0fef19f04c51307295c00cdbb61
vegajorge <vegajorgeluis@live.com>
2024-07-10 12:20:20 -0300

    proyecto allura react

---
+++ alura-flix/src/api/api.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+
+const baseURL = 'https://668e9728bf9912d4c92ef261.mockapi.io/api';
+
+
+
+const api = axios.create({
+  baseURL: baseURL,
+});
+
+export const obtenerDatos = async (url, setData) => {
+    try {
+        const respuesta = await api.get(`${baseURL}${url}`)
+        setData(respuesta.data)
+    } catch (error) {
+        alert('Error al consultar los datos: ' + error.message); 
+    }
+}
+
+export const enviarDatos = async (url, datos) => {
+    try {
+        const respuesta = await api.post(url, datos); 
+        alert('Datos actualizados correctamente');
+        console.log('Respuesta del servidor:', respuesta.data); 
+    } catch (error) {
+        alert('Error al actualizar los datos: ' + error.message); 
+    }
+}
+
+export const actualizarDatos = async (url, datos) => {
+    try {
+        const respuesta = await api.put(url, datos); 
+        alert('Datos actualizados correctamente');
+        console.log('Respuesta del servidor:', respuesta.data);
+    } catch (error) {
+        alert('Error al actualizar los datos: ' + error.message);
+    }
+}
+
+export const eliminarDatos = async (id) => {
+    try {
+        const respuesta = await axios.delete(`${baseURL}${id}`)
+        console.log(`${baseURL}${id}`)
+        // alert('Los datos se han eliminado!',respuesta,`${baseURL}${id}`)
+    } catch (error) {
+        alert('Error al eliminar los datos')
+    }
+}
---
+++ alura-flix/src/pages/Page404.jsx
@@ -0,0 +1,9 @@
+const Page404 = () =>{
+
+    return (
+           <p>Page404 no pudo encontrarse la pagina!!!</p>
+    )
+    
+}
+
+export default Page404
